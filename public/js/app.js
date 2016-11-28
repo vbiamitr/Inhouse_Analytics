4cf660d63f37048540830be2ca0138b79a89e94b
vbiamitr <amitroy.github@gmail.com>
2016-11-28 15:09:42 -0600

    Advanced search UI and angular binding done

--- public/js/app.js
+++ public/js/app.js
@@ -37,6 +37,24 @@ ih_app.factory('configService', ['$http', function($http){
 ih_app.factory('companyService',['$http', 'configService', function($http, configService){
     var service = {};
 
+    service.fields = [{"key":"company","name":"Company"},{"key":"domain","name":"Domain"},{"key":"address","name":"Address"},{"key":"city","name":"City"},{"key":"state","name":"State"},{"key":"zipcode","name":"Zipcode"},{"key":"country","name":"Country"},{"key":"industry","name":"Industry"},{"key":"sic_code","name":"SIC Code"},{"key":"revenue","name":"Revenue"},{"key":"employees","name":"Employees"},{"key":"software","name":"Software"},{"key":"parent","name":"Parent"}];  
+
+    service.fieldInfo = {
+        'company' : 'Name of the company',
+        'domain' : 'Domain name of the company',
+        'address' : 'Street Address of the company',
+        'city' : 'Name of the city',
+        'state' : 'Name of the state/province',
+        'zipcode' : 'Zip Code or Postal Code',
+        'country' : 'Name of the country',
+        'industry' : 'Type of industry',
+        'sic_code' : 'SIC (Standard Industrial Classification) code of the industry',
+        'revenue' : 'Revenue of the company',
+        'employees' : 'No. of employees in the company',
+        'software' : 'Softwares being used by the company.Eg. "BO,BI,BOBJ" ',
+        'parent' : 'The parent company'
+    };
+
     service.getCompany = function(options, cb){        
         var urlParams = [configService.server_base_url ,'getcompany', options.skip, options.limit];
         if(typeof options.search != "undefined"){
@@ -68,8 +86,8 @@ ih_app.config(function($routeProvider) {
     .otherwise("/");
 });
 
-ih_app.controller('uploadController', ['$scope', 'Upload', '$timeout', '$routeParams', 'configService', function ($scope, Upload, $timeout, $routeParams, configService) {
-    
+ih_app.controller('uploadController', ['$scope', 'Upload', '$timeout', '$routeParams', 'configService', 'companyService',  function ($scope, Upload, $timeout, $routeParams, configService, companyService) {
+    $scope.fields = companyService.fields;
     var url = configService.server_base_url + '/uploads/xlsx';
     $scope.type = $routeParams.type;
     $scope.uploadFiles = function (files) {
@@ -94,6 +112,12 @@ ih_app.controller('uploadController', ['$scope', 'Upload', '$timeout', '$routePa
             });
         }
     };
+    $scope.getFieldInfo = function(field){
+        if(field && companyService.fieldInfo){
+            $scope.fieldInfo = companyService.fieldInfo[field] || '';
+            $scope.selectedField = field;
+        }
+    }
 }]);
 
 ih_app.controller('viewController', ['$scope', 'companyService', function ($scope, companyService) {
@@ -102,6 +126,10 @@ ih_app.controller('viewController', ['$scope', 'companyService', function ($scop
     $scope.stopScrolling = !1;
     $scope.cursor_total = 0;
     $scope.companies = [];
+    $scope.companyInfo = {};
+    $scope.showInfo = false;
+    $scope.selectedCompany = "";
+    $scope.fields = companyService.fields;
     var options = {
         skip : $scope.cursor_skip,
         limit : $scope.cursor_limit
@@ -160,6 +188,8 @@ ih_app.controller('viewController', ['$scope', 'companyService', function ($scop
         $scope.stopScrolling = !1;
         $scope.cursor_total = 0;
         $scope.companies = [];
+        $scope.showInfo = false;
+        $scope.selectedCompany = "";
         var options = {
             skip : $scope.cursor_skip,
             limit : $scope.cursor_limit
@@ -187,5 +217,23 @@ ih_app.controller('viewController', ['$scope', 'companyService', function ($scop
                 $scope.cursor_total = result.cursor_total;       
             }
         });              
+    }
+
+    $scope.getInfo = function (company){
+        $scope.showInfo = true;
+        $scope.companyInfo = company;
+        $scope.selectedCompany = company.domain;
+    }
+
+    $scope.advancedSearch = function(){
+        var fields = $scope.fields;
+        var search_obj = {};
+        fields.forEach(function(field){
+            var val = $scope['input_' + field.key];
+            if(val){
+                search_obj[field.key] = val;
+            }
+        });
+        console.log(JSON.stringify(search_obj));
     }
 }]);
